b1c356dd3fd05c336bf0b0a6d2964377c3eaffc1
Kelsey-Hansel <kelseylhansel@gmail.com>
2025-12-12 20:34:36 -0600

    Progress on my final project.

--- final-project/data/books.mjs
+++ final-project/data/books.mjs
@@ -56,52 +56,52 @@ export const books = [
         "coverImage": "images/dungeons-and-drama.jpg"
     },
     {
-        "title": "",
-        "author": "",
-        "genre": "",
-        "description": "",
-        "coverImage": ""
+        "title": "Divergent",
+        "author": "Veronica Roth",
+        "genre": "Dystopian",
+        "description": "For sixteen-year-old Tris, the world changes in a heartbeat when she is forced to make a terrible choice. Turning her back on her family, Tris ventures out, alone, determined to find out where she truly belongs. Shocked by the brutality of her new life, Tris can trust no one.And yet she is drawn to a boy who seems to both threaten and protect her.The hardest choices may yet lie ahead…",
+        "coverImage": "images/divergent.jpg"
     },
     {
-        "title": "",
-        "author": "",
-        "genre": "",
-        "description": "",
-        "coverImage": ""
+        "title": "The Maze Runner",
+        "author": "James Dashner",
+        "genre": "Dystopian",
+        "description": "If you ain't scared, you ain't human. When Thomas wakes up in the lift, the only thing he can remember is his name.He's surrounded by strangers—boys whose memories are also gone. Nice to meet ya, shank.Welcome to the Glade. Outside the towering stone walls that surround the Glade is a limitless, ever- changing maze.It's the only way out—and no one's ever made it through alive. Everything is going to change. Then a girl arrives.The first girl ever.And the message she delivers is terrifying. Remember.Survive.Run.",
+        "coverImage": "images/maze-runner.jpg"
     },
     {
-        "title": "",
-        "author": "",
-        "genre": "",
-        "description": "",
-        "coverImage": ""
+        "title": "Inkheart",
+        "author": "Cornelia Funke",
+        "genre": "Fantasy",
+        "description": "One cruel night, Meggie's father reads aloud from a book called INKHEART-- and an evil ruler escapes the boundaries of fiction and lands in their living room. Suddenly, Meggie is smack in the middle of the kind of adventure she has only read about in books. Meggie must learn to harness the magic that has conjured this nightmare. For only she can change the course of the story that has changed her life forever.",
+        "coverImage": "images/inkheart.jpg"
     },
     {
-        "title": "",
-        "author": "",
-        "genre": "",
-        "description": "",
-        "coverImage": ""
+        "title": "The Hunger Games",
+        "author": "Suzanne Collins",
+        "genre": "Dystopian",
+        "description": "Winning means fame and fortune. Losing means certain death. The Hunger Games have begun. . . . In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts.The Capitol is harsh and cruel and keeps the districts in line by forcing them all to send one boy and one girl between the ages of twelve and eighteen to participate in the annual Hunger Games, a fight to the death on live TV. Sixteen-year-old Katniss Everdeen regards it as a death sentence when she steps forward to take her sister's place in the Games. But Katniss has been close to dead before-and survival, for her, is second nature. Without really meaning to, she becomes a contender. But if she is to win, she will have to start making choices that weigh survival against humanity and life against love.",
+        "coverImage": "images/hunger-games.jpg"
     },
     {
-        "title": "",
-        "author": "",
-        "genre": "",
-        "description": "",
-        "coverImage": ""
+        "title": "The Eleventh Hour",
+        "author": "Graeme Base",
+        "genre": "Mystery",
+        "description": "When Horace the elephant turns eleven, he celebrates in style by inviting his exotic friends to a splendid costume party. But a mystery is afoot, for in the midst of the games, music, and revelry, someone has eaten the birthday feast. The rhyming text and lavish, detailed illustrations each provide clues, and it's up to the reader to piece them together and decide whodunit!",
+        "coverImage": "images/eleventh-hour.jpg"
     },
     {
-        "title": "",
-        "author": "",
-        "genre": "",
-        "description": "",
-        "coverImage": ""
+        "title": "The Giver",
+        "author": "Lois Lowry",
+        "genre": "Dystopian",
+        "description": "Life in the community where Jonas lives is idyllic. Designated birthmothers produce new children, who are assigned to appropriate family units; one male, one female, to each. Citizens are assigned their partners and their jobs. No one thinks to ask questions. Everyone obeys. Their community is a precisely choreographed world without conflict, inequality, divorce, unemployment, injustice ... or choice. Everyone is the same. Except Jonas.",
+        "coverImage": "images/giver.jpg"
     },
     {
-        "title": "",
-        "author": "",
-        "genre": "",
-        "description": "",
-        "coverImage": ""
+        "title": "Red Rising",
+        "author": "Pierce Brown",
+        "genre": "Dystopian",
+        "description": "Darrow is a Red, a member of the lowest caste in the color-coded society of the future. Like his fellow Reds, he works all day, believing that he and his people are making the surface of Mars livable for future generations. Yet he toils willingly, trusting that his blood and sweat will one day result in a better world for his children. But Darrow and his kind have been betrayed. Soon he discovers that humanity reached the surface generations ago. Vast cities and lush wilds spread across the planet. Darrow—and Reds like him—are nothing more than slaves to a decadent ruling class.",
+        "coverImage": "images/red-rising.jpg"
     }
 ]
